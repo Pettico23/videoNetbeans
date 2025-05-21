2d827fcc7a8ce15d5b957b878ffe24e913ca2000
Rodrigo Pettico <rodrigo.pettico@gmail.com>
2025-05-21 10:04:54 -0300

    EJEMPLO USO DE  NETBEANS

---
+++ src/ejercicio01/Ejercicio01.java
@@ -0,0 +1,18 @@
+
+package ejercicio01;
+
+
+public class Ejercicio01 {
+
+    
+    public static void main(String[] args) {
+        
+       Rectangulo unRectangulo = new Rectangulo(2,5);
+       
+       //Salida a consola
+        System.out.println("El area es: " + unRectangulo.calcularArea());
+        System.out.println("El perimetro es:" + unRectangulo.);
+        System.out.println("¿Es Cuadrado?" + unRectangulo.esCuadrado());
+    }
+    
+}
---
+++ src/ejercicio01/Rectangulo.java
@@ -0,0 +1,51 @@
+
+package ejercicio01;
+
+
+public class Rectangulo {
+    private double ancho;
+    private double alto;
+    
+    
+    public Rectangulo(double ancho, double alto){
+        this.ancho = ancho;
+        this.alto = alto;
+    
+    }
+
+    public double getAncho() {
+        return ancho;
+    }
+
+    public void setAncho(double ancho) {
+        this.ancho = ancho;
+    }
+
+    public double getAlto() {
+        return alto;
+    }
+
+    public void setAlto(double alto) {
+        this.alto = alto;
+    }
+    
+    public double calcularArea(){
+        double result;
+        result = alto * ancho;
+        return result;
+    
+    }
+    public double calularPerimetro{
+        double result
+        result = 
+        return result;
+        
+
+    }
+    public boolean esCuadrado(){
+    
+        boolean result;
+        result = ancho == alto;
+        return result;
+    }
+}
